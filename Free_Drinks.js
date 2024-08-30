7d490de9d10f982636e0b7a3520ace7bdf7c1e85
TanzidAnan <tanzid.anan125@gmail.com>
2024-08-31 02:54:21 +0600

    Free Drink

---
+++ Free_Drinks.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+const bargerPrice = 500;
+
+if(bargerPrice >= 500){
+    console.log('free coke')
+}
+else{
+    console.log('Else coke 300 tk')
+}
