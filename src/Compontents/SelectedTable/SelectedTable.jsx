a5fbec396bb97f186df4b005cd760f93b9251dc4
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-04 01:32:21 +0600

    data delete

--- src/Compontents/SelectedTable/SelectedTable.jsx
+++ src/Compontents/SelectedTable/SelectedTable.jsx
@@ -1,8 +1,19 @@
 /* eslint-disable react/prop-types */
 
-const SelectedTable = ({ data,idx }) => {
-    const { day, formatHour, formattedData, title } = data
-    console.log(data)
+const SelectedTable = ({ data,idx,setSeduleData,seduleData }) => {
+    const { _id,day, formatHour, formattedData, title } = data
+    console.log(data);
+    const hendleDelete =(id) =>{
+        console.log(id)
+        fetch(`http://localhost:5000/schedule/${id}`,{
+            method:'DELETE',
+        })
+        .then(res =>res.json())
+        .then(data =>{
+            const newData =seduleData.filter((sedule) => sedule._id !== id);
+            setSeduleData(newData)
+        })
+    }
     const isCompleted = true
     return (
         <div>
@@ -14,7 +25,7 @@ const SelectedTable = ({ data,idx }) => {
                 <td>{formattedData}</td>
                 <td className="flex gap-6">
                     <button className="btn"><i className="fa-solid fa-pen-to-square"></i></button>
-                    <button className="btn"><i className="fa-solid fa-trash"></i></button>
+                    <button onClick={() =>hendleDelete(_id)} className="btn"><i className="fa-solid fa-trash"></i></button>
                     <button className="btn"><i className="fa-solid fa-check"></i></button>
                 </td>
             </tr>
--- src/Compontents/Selectede/Selectede.jsx
+++ src/Compontents/Selectede/Selectede.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-undef */
 import { useLoaderData } from "react-router-dom";
 import SelectedTable from "../SelectedTable/SelectedTable";
+import { useState } from "react";
 
 const Selectede = () => {
 
     const AllData =useLoaderData();
     console.log(AllData)
+    const [seduleData,setSeduleData] =useState(AllData)
 
     return (
         <div className="w-9/12 mx-auto">
@@ -27,7 +29,10 @@ const Selectede = () => {
                         </table>
                         <tbody>
                             {
-                              AllData.length===0?<p>No data found</p> :AllData.map((data,index) => <SelectedTable  key={data._id} idx={index} data={data}></SelectedTable>) 
+                              seduleData.length===0?<p>No data found</p> :seduleData.map((data,index) => <SelectedTable  key={data._id} idx={index}
+                              setSeduleData={setSeduleData}
+                              seduleData={seduleData}
+                              data={data}></SelectedTable>) 
                             }
                         </tbody>
                     </div>
